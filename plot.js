d837fb152c45f6e66d052474df04c0c58af39063
Kristin <coldzonemt@yahoo.com>
2015-09-13 13:38:35 -0700

    implement basic axis for d3 math plotting program

---
+++ plot.js
@@ -0,0 +1,57 @@
+var svgH = 500;
+var svgW = 500;
+
+//to get the parabola to point upwards, the y axis values must be multiplied by -1
+var dataSet = [[0,0], [1,-1], [-1,-1], [2,-4], [-2,-4], [3,-9], [-3,-9]];
+
+var xScale = d3.scale.linear()
+  .domain([-10, 10])
+  .range([0, svgW]);
+
+var yScale = d3.scale.linear()
+  .domain([-10, 10])
+  .range([0, svgH]);
+
+//create the svg
+var svg = d3.select('body')
+  .append('svg')
+  .attr('width', svgW+50)
+  .attr('height', svgH+50)
+  .append('svg:g') //"svg:g" is significant here - why?
+  .attr('transform', 'translate(30,30)'); //define these
+
+//"build" the circles
+svg.selectAll('circle')
+  .data(dataSet)
+  .enter()
+    .append('circle')
+    .attr('cx', function(d) {
+      return xScale(d[0]);
+    })
+    .attr('cy', function(d) {
+      return yScale(d[1]);
+    })
+    .attr('r', 5);
+
+svg.append('svg:g')
+  .attr('class', 'xaxis')
+  .attr('class', 'yaxis');
+
+var xAxis = d3.svg.axis() //svg.axis() is a d3 method!
+  .scale(xScale)
+  .orient('center')
+  .tickSize(1);
+
+var yAxis = d3.svg.axis()
+  .scale(yScale)
+  .orient('left')
+  .tickSize(1);
+
+svg.append('svg:g')
+  .attr('class', 'xaxis')
+  .attr('transform', 'translate(0, ' + svgW + ')')
+  .call(xAxis);
+
+svg.append('svg:g')
+  .attr('class', 'yaxis')
+  .call(yAxis);
